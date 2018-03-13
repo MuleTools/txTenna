de3cf4b67158d139902705cbab81cb81ec4d3f9b
Sarath kumar <sarathkumars677@gmail.com>
2018-03-13 23:14:48 +0530

    Added Networking activity

---
+++ app/src/main/java/com/samourai/txtenna/Networking.java
@@ -0,0 +1,13 @@
+package com.samourai.txtenna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Networking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_networking);
+    }
+}
